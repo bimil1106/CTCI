b2265156fa1cd4210d3bde82912ff26cf2be3b17
bimilnoosul <jinsunlee@gmail.com>
2012-08-16 00:21:53 -0700

    first checkin

---
+++ JinsunSolution/src/google/top150/TransposeMatrix.java
@@ -0,0 +1,10 @@
+package google.top150;
+
+public class TransposeMatrix {
+	/*
+	 * input: a1, a2, ..., an, b1, b2, ..., bn, c1, c2, ..., cn
+	 * output: a1, b1, c1, a2, b2, c2, ..., an, bn, cn
+	 * the transpose should be done in place
+	 */
+
+}
