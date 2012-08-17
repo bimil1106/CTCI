ef042fccda91224d928c9913d0d5239d314ec470
chestkim <violkim@gmail.com>
2012-08-16 17:15:39 -0700

    different use case.  succeed!

--- ChesterSolution/src/google/top150/transposeinplace/TransposeInPlaceTest.java
+++ ChesterSolution/src/google/top150/transposeinplace/TransposeInPlaceTest.java
@@ -6,12 +6,12 @@ public class TransposeInPlaceTest {
 
 	@Test
 	public void testTranspose() {
-		int width = 6;
+		int width = 7;
 		int height = 4;
-		int[] input = { 10, 20, 30, 40, 50, 60,
-						11, 21, 31, 41, 51, 61,
-						12, 22, 32, 42, 52, 62,
-						13, 23, 33, 43, 53, 63};
+		int[] input = { 10, 20, 30, 40, 50, 60, 70,
+						11, 21, 31, 41, 51, 61, 71,
+						12, 22, 32, 42, 52, 62, 72,
+						13, 23, 33, 43, 53, 63, 73};
 		int[] result = TransposeInPlace.transpose(input, width, height);
 		int inx = 0;
 		for (int i=0; i<width; i++) {
