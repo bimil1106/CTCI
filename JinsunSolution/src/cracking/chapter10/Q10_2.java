c9a243af7ed1fa1140a91bd456bcdb94ab8c03f4
bimilnoosul <jinsunlee@gmail.com>
2012-11-21 23:40:45 -0800

    chap10 answers

--- JinsunSolution/src/cracking/chapter10/Q10_2.java
+++ JinsunSolution/src/cracking/chapter10/Q10_2.java
@@ -9,5 +9,52 @@ package cracking.chapter10;
  */
 
 public class Q10_2 {
-
+/*
+ * Step1: Simplify the problem - forget about the millions of Users
+ * 		Construct Graph: a node - each person, an edge - the two users are friends
+ * 		class Person {
+ * 			Person[] friends;
+ * 			// Other info
+ * 		}
+ * 		How to find the connection between two people:
+ * 			Breadth first search
+ * 		Why not Depth first search? inefficient
+ * 			two users might be only one degree of separation apart
+ *
+ * Step2: Handle the Millions of Users
+ * 		Multiple machines are needed to keep data
+ * 		In step1, we assumed all data are in the one machine
+ * 		Replace our list of friends with a list of their IDs.
+ * 		How to traverse: 
+ * 			1. For each friend ID: int machine_index = getMachineIDForUser(person_id);
+ * 			2. Go to machine #machine_index
+ * 			3. On that machine, do: Person friend = getPersonWithID(person_id);
+ * 
+ * 		Optimization: Reduce expensive Machine Jumps
+ * 			by batching these jumps - 
+ * 				e.g.if five of my friends live on one machine, look them up all at once
+ * 
+ * 		Optimization: Smart Division
+ * 			devide them up by country, city, state and so on.
+ * 
+ * 		Question: BFS usually requires marking a node as visited. how do you do that in this case?
+ * 			expensive marking data --> mimic the marking of nodes with a hash table 
+ * 				to look up a node id whether or not it's been visited
+ * 			
+ * 		See the following questions: p347
+ * 			> In real world, servers fail. How does this affect you?
+ * 				make a backup machine located in a different machine.
+ * 			> How could you take advantage of caching?
+ * 				make a caching layer in our machine if machine jumps happens for finding friends
+ * 				(who is going to be cached? common or popular person in this machine)
+ * 			> Do you search until the end of the graph (infinite)? How do you decide when to give up?
+ * 				give up when you want to give up.. depending of your purpose..
+ * 			> In real life, some people have more friends of friends than others, and are therefore
+ * 			more likely to make a path between you and someone else. How could you use this data
+ * 			to pick where you start traversing?
+ * 				if someone has more friends, then there is more possibility to find some others
+	            So sort friends with more friends, then do the DFS using the friend.
+ * 
+ * 
+ */	                     
 }
--- JinsunSolution/src/cracking/chapter10/Q10_5.java
+++ JinsunSolution/src/cracking/chapter10/Q10_5.java
@@ -6,5 +6,30 @@ package cracking.chapter10;
  */
 
 public class Q10_5 {
+	/*
+	 *  how an infinite loop might occur?
+	 *      picture the web as a graph of links, if a cycle, then infinite loop
+	 * 
+	 *  to prevent infinite loops -> need to detect cycles
+	 *      create hash table where we set hash[v] to true after we visit page v.
+	 *          By BFS, each time we visit a page, we gather all its links and insert them at the end of a queue
+	 *          If we've already visited a page, we ignore it.
+	 *  
+	 *  How does it means to visit page v? based on content or URL?
+	 *      But it is tricky to define a "different page"
+	 *      So, some sort of estimation for degree of similarity.
+	 *      If a page is deemed to be sufficiently similar to other pages, we deprioritize crawling its children
+	 *      For each page, come up with some sort of signature based on snippets of the content and page's URL
+	 *          How work?
+	 *          1. Open up the page and create a signature of the page based on specific subsections of the page content and its URL
+	 *          2. Query the db to see whether anything with this signature has been crawled recently
+	 *          3. If something with this signature has been recently crawled, insert this page back into the database at a low priority
+	 *          4. If not, crawl the page and insert its links into the db.
+	 *  
+	 *  By this approach, we never "complete" crawling the web, but we will avoid getting stuck in a loop of pages.
+	 * 
+	 * 
+	 * 
+	 */
 
 }
